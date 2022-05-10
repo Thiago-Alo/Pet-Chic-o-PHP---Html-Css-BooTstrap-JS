f17b45f0e20f7834af0ea77bfe739bd583ffeb35
Thiago Alô <thiago_kadri@hotmail.com>
2022-05-10 10:37:17 +0100

    add img and links

--- content/pages/inicio.php
+++ content/pages/inicio.php
@@ -89,13 +89,13 @@
         <div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
           <div class="carousel-inner radios">
             <div class="carousel-item active">
-              <a href="promocoes.html">
+              <a href="home.php?p=produtos">
               <img src="img/Promoções/acana.jpg" height="750" width="750" class="d-block w-100" alt="..."><body>
               </body>
               </html>
             </div>
             <div class="carousel-item">
-              <a href="promocoes.html">
+              <a href="home.php?p=produtos">
               <img src="img/Promoções/turtle.jpg" height="750" width="750" class="d-block w-100" alt="..."></a>
             </div>
           </div>
@@ -114,15 +114,15 @@
       <!--LADO DIREITO, PRODUTOS-->
       <div class="ld-direito">
         <h2>PRODUTOS</h2>
-        <img src="img/produtos/racao.jpg" alt=""><a href=""></a></img>
-        <img src="img/produtos/catfood.png" alt=""><a href=""></a></img>
+        <a href="home.php?p=produtos"><img src="img/produtos/racao.jpg" alt=""></img></a>
+        <a href="home.php?p=produtos"><img src="img/produtos/catfood.jpg" alt=""></img></a>
 
         <div id="cont" class="contatos">
           <p>Nossos Contactos:</p>
-          <i class="fab fa-facebook"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-whatsapp"></i>
-          <i class="fab fa-instagram"></i>
+          <a href="home.php?p=contactos"><i class="fab fa-facebook"></i></a>
+          <a href="home.php?p=contactos"><i class="fab fa-twitter"></i></a>
+          <a href="home.php?p=contactos"><i class="fab fa-whatsapp"></i></a>
+          <a href="home.php?p=contactos"><i class="fab fa-instagram"></i></a>
         </div>
         <a href="https://www.google.com/maps/place/CESAE+-+Centro+de+Servi%C3%A7os+e+Apoio+%C3%A0s+Empresas/@41.1585147,-8.6515179,17z/data=!4m5!3m4!1s0xd246582df7d8785:0xc967e42932cd22b4!8m2!3d41.1585107!4d-8.6502623" >
         <img class="w-100 h-auto"src="img/morada.png" alt="Morada" title="Morada"></a>
